73f59def46f3145f3ae2af4189e9736ac4169687
Rafaelacss <rsilvei2@my.centennialcollege.ca>
2017-07-27 22:16:01 -0400

    enum face created

---
+++ Highest Cards/Face.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Rafaela Silveira
+ * Date: July 27, 2017
+ * Description: Face Enum
+ * Version: 0.1 
+ */
+
+namespace Highest_Cards
+{
+    public enum Face 
+    {
+        Ace = 1, Two, Three, Four, Five, Six,
+        Seven, Eight, Nine, Ten, Jack, Queen, King
+    }
+}
