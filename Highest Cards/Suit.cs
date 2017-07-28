11006bf96bffc0919ae4a09be1d5b02ea62424c3
Rafaelacss <rsilvei2@my.centennialcollege.ca>
2017-07-27 22:19:03 -0400

    enum suit created

---
+++ Highest Cards/Suit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Rafaela Silveira
+ * Date: July 27, 2017
+ * Description: Suit Enum
+ * Version: 0.1 
+ */
+
+namespace Highest_Cards
+{
+    public enum Suit
+    {
+        Clubs, Diamonds, Hearts, Spades
+    }
+}
